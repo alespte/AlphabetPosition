5f7535d517d51f72d6202f04a600c508216dc267
Alexander Hernández <111263111+alespte@users.noreply.github.com>
2022-09-05 22:49:01 -0500

    Add files via upload

---
+++ codewars_csharp_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+public static class Program
+{
+    public static string AlphabetPosition (string text){
+        text = text.ToUpper();
+        byte [] ascii_letters = Encoding.ASCII.GetBytes(text);
+        text = "";
+
+        for (int i = 0; i < ascii_letters.Length; i++)
+        {
+            int temp = (ascii_letters[i] - 64);
+            string text_temp;
+            if (temp <= 0 || temp > 26)
+            {
+                text_temp = " ";    
+            }
+            else
+            {   
+                text_temp = temp.ToString() + " ";
+                text += text_temp;    
+            }
+            //Console.WriteLine(text);
+        }
+        Console.WriteLine(text);
+        return text.Trim();
+    }
+
+    static void Main (string [] args){
+        string text = ("");
+        AlphabetPosition(text);
+    }
+    
+}
+
+
